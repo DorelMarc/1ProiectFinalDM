adb17c3a6ac4a054fb18d02b044cc27db3fccf6d
D.Marc <dorel_marc@live.com>
2024-07-31 13:50:34 +0300

    Update AddProductToCart.java

--- src/test/java/AddProductToCart.java
+++ src/test/java/AddProductToCart.java
@@ -19,15 +19,35 @@ public class AddProductToCart {
         driver.manage().window().maximize();
 
     }
-    @Test
-    public void addToCartTest(){
-       //open the product page
-        WebElement openthepage=driver.findElement(By.xpath("/html/body/app-root/div/app-overview/div[3]/div[2]/div[1]/a[2]/div[2]/h5"));
-        openthepage.click();
+   @Test
+    public void addToCartTest() {
+        //open product page
+        Wait<WebDriver> waitproduct = new WebDriverWait(driver, Duration.ofSeconds(10));
+        waitproduct.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/app-root/div/app-overview/div[3]/div[2]/div[1]/a[1]")));
+        WebElement productPage = driver.findElement(By.xpath("/html/body/app-root/div/app-overview/div[3]/div[2]/div[1]/a[1]"));
+        productPage.click();
         // add to cart
-        Wait<WebDriver> wait= new WebDriverWait(driver, Duration.ofSeconds(10));
-        WebElement addToCartButton= driver.findElement(By.xpath("//*[@id=\"btn-add-to-cart\"]"));
+        Wait<WebDriver> wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("btn-add-to-cart")));
+        WebElement addToCartButton = driver.findElement(By.id("btn-add-to-cart"));
         addToCartButton.click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"lblCartCount\"]")));
+        WebElement cartNumber = driver.findElement(By.xpath("//*[@id=\"lblCartCount\"]"));
+        String expectedCartNumber = "1";
+        Assert.assertTrue(cartNumber.getText().contains(expectedCartNumber));
+        // open cart
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"navbarSupportedContent\"]/ul/li[5]/a/fa-icon")));
+        WebElement cartIcon = driver.findElement(By.xpath("//*[@id=\"navbarSupportedContent\"]/ul/li[5]/a/fa-icon"));
+        cartIcon.click();
+
+        // Clear the cart
+        Wait<WebDriver> wait3=new WebDriverWait(driver, Duration.ofSeconds(30));
+        wait3.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/app-root/div/app-checkout/aw-wizard/div/aw-wizard-step[1]/app-cart/div/table/tbody/tr/td[5]/a")));
+        WebElement clearCart=driver.findElement(By.xpath("/html/body/app-root/div/app-checkout/aw-wizard/div/aw-wizard-step[1]/app-cart/div/table/tbody/tr/td[5]/a"));
+        clearCart.click();
+        Wait<WebDriver> wait2= new WebDriverWait(driver, Duration.ofSeconds(20));
+        wait2.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//*[@id=\"lblCartCount\"]")));
+        Assert.assertFalse(cartIcon.isDisplayed());
 
     }
 }
